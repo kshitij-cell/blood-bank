ef44f8b5e3d6234458beec781e66bfc7b799ac6d
Kshitij-cel <kshitijkanth2000@gmail.com>
2021-07-12 20:29:47 +0530

    homepage

--- header.js
+++ header.js
@@ -1,13 +1,13 @@
-const links = document.querySelectorAll('.link-');
-let old = 0;
-links[0].classList.add('active');
-for (let index = 0; index < links.length; index++) {
-    links[index].addEventListener('click', function () {
-        links[index].classList.add('active');
-        links[old].classList.remove('active');
-        old = index;
-    })
-};
+// const links = document.querySelectorAll('.link-');
+// let old = 0;
+// links[0].classList.add('active');
+// for (let index = 0; index < links.length; index++) {
+//     links[index].addEventListener('click', function () {
+//         links[index].classList.add('active');
+//         links[old].classList.remove('active');
+//         old = index;
+//     })
+// };
 const header = document.querySelector(".header-.container-");
 document.addEventListener("scroll", () => {
     var scroll_pos = window.scrollY;
