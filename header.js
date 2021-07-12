8fa5bea0add6bb4bc8590d2bb4ee7807adf8ed8a
Kshitij-cel <kshitijkanth2000@gmail.com>
2021-07-12 16:59:07 +0530

    1st page

---
+++ header.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('.link-');
+let old = 0;
+links[0].classList.add('active');
+for (let index = 0; index < links.length; index++) {
+    links[index].addEventListener('click', function () {
+        links[index].classList.add('active');
+        links[old].classList.remove('active');
+        old = index;
+    })
+};
+const header = document.querySelector(".header-.container-");
+document.addEventListener("scroll", () => {
+    var scroll_pos = window.scrollY;
+    if (scroll_pos > 250) {
+        header.style.backgroundColor = "black";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
